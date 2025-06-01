20427c76bedd96b4024e9db6dfa9acfc205f3e62
Prams43 <najwanhp123@gmail.com>
2025-06-01 20:54:08 +0700

    new.php

--- new.php
+++ new.php
@@ -1,10 +1,9 @@
-
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>testis</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <script>
  let total = 0;
@@ -56,9 +55,9 @@ function sudahBayar(){
 
 </script>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+    <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand">Kantin Sekolah</a>
+    <a class="navbar-brand" href="#">Kantin</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -68,82 +67,158 @@ function sudahBayar(){
           <a class="nav-link active" aria-current="page" href="#kantin">About Kantin</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#cafetaria">Cafetaria List</a>
+          <a class="nav-link" href="#list ">Cafetaria List</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#htb">How To Buy</a>
+          <a class="nav-link" href="#">How to Buy</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#contact">Contact Us</a>
+         <li class="nav-item">
+          <a class="nav-link" href="#contact ">Contact Us</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
-<section id="kantin">
-<h1 class="ms-5 mt-5">KANTIN SEKOLAH</h1>
-<div class="ms-5 mt-5 me-5" >
-<img src="Kantin.jpg" alt="Italian Trulli" width="100%" height="100%"> 
-</div>
-
-<div class="ms-5 mt-5" >
-<iframe width="560" height="315" src="https://www.youtube.com/embed/2GaPSBNJNbk?si=ktP1VgjiZb8YDOL4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    </div>
-    <div class="ms-5 mt-5 mb-5" >
-    <pre>Kantin adalah tempat yang menyediakan makanan dan minuman, biasanya di sekolah, kantor, atau tempat umum lainnya. 
-Kantin dapat menjadi tempat untuk membeli makanan dan minuman, atau bisa juga menjadi tempat untuk makan jika ada fasilitas tempat duduk</pre>
-</div>
+<section id="kantin" class="mt-5 ms-5 me-5">
+  <h1>KANTIN TELKOM</h1>
+  <img src="gmbrkntin.jpg" width="80%" height="80%" style="display: absolute">
 </section>
-<section id ="cafetaria">
-<h1 class="ms-5 mt-5 ">Cafetaria List</h1>
-<div class="card mb-3 mt-5 ms-5" style="max-width: 600px;">
-  <div class="row no-gutters">
-    <div class="col-md-5">
-      <img src="katsu.jpg" alt="katsu" class="img-fluid h-100">
-    </div>
-    <div class="col-md-5">
-      <div class="card-body">
-        <h5 class="card-title">Katsu Bu Ida</h5>
-        <p class="card-text">Menjual Aneka Katsu.</p>
-        <pre class="card-text">Menu Makanan:
-Katsu Tiramisu
-Katsu Coklat
+<div>
+<img src="logokntin.png" width="30%" height="50%" style="display: absolute; top: 500px">
+</div>
 
-Menu Minuman:
-Esteh
-Es Coklat
-        </pre>
+<div>
+<pre class="ms-5 mx-5"style="left: 600px;">Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+Provident asperiores rem soluta odit error, 
+consequatur corporis temporibus repudiandae odio ut nobis dignissimos sequi facere eligendi libero reiciendis iusto iste voluptatibus.
+<pre>
+</div>
+<iframe class="ms-5 mx-5"width="560" height="315" src="https://www.youtube.com/embed/SHP-xh5NnVs?si=FFZZmIYH9tEqPQb5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<section class="ms-5 mx-5 mt-5"id="list">
+  <h1>Cafetaria List</h1>
+  <div class="card mb-3 mt-3" style="max-width: 1000px;">
+  <div class="row no-gutters">
+    <div class="col-md-3">
+      <img src="kantin1.jpg" alt="...">
+    </div>
+    <div class="col-md-7">
+      <div class="card-body">
+        <h5 class="card-title">Pak Rusdi</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Seger Banget Coy!</small></p>
       </div>
     </div>
   </div>
 </div>
-<div class="card mb-5 mt-5 ms-5" style="max-width: 600px;">
+<div class="card mb-3 mt-3" style="max-width: 1000px;">
   <div class="row no-gutters">
-    <div class="col-md-5">
-      <img src="katsu.jpg" alt="katsu" class="img-fluid h-100">
+    <div class="col-md-3">
+      <img src="kantin1.jpg" alt="...">
     </div>
-    <div class="col-md-5">
+    <div class="col-md-7">
       <div class="card-body">
-        <h5 class="card-title">Bubur Pak Narji</h5>
-        <p class="card-text">Menjual Aneka Bubur.</p>
-        <pre class="card-text">Menu Makanan:
-Bubur Ayam Bakar
-Bubur Bebek Bakar
-
-Menu Minuman:
-Es Kelapa
-Es Durian
-        </pre>
+        <h5 class="card-title">Bu Dwi</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Ayo Dibeli!</small></p>
       </div>
     </div>
   </div>
+</div>
+<h2 class="mt-5">Menu Pak Rusdi</h2>
+
+<div class="row">
+
+<div class="col">
+     <div class="card" style="width: 18rem;">
+  <img src="katsu1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Katsu Sigma</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+
+    <div class="col">
+       <div class="card" style="width: 18rem;">
+  <img src="katsu2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Katsu Bakar</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+
+<div class="col">
+       <div class="card" style="width: 14rem;">
+  <img src="escoklat.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Es Coklat</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+
+    <div class="col">
+       <div class="card" style="width: 18rem;">
+  <img src="esteh.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Esteh</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+    </div>
+</div>
+</div>
+
+<h2 class="mt-5">Menu Bu Dwi</h2>
+
+<div class="row">
+
+<div class="col">
+     <div class="card" style="width: 18rem;">
+  <img src="katsu1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Ayam Goyeng Sigma</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+
+    <div class="col">
+       <div class="card" style="width: 18rem;">
+  <img src="katsu2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Sushi Patih</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+
+<div class="col">
+       <div class="card" style="width: 14rem;">
+  <img src="escoklat.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Teh Botol</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+
+    <div class="col">
+       <div class="card" style="width: 18rem;">
+  <img src="esteh.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Teh Segitiga</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+    </div>
+</div>
 </div>
 </section>
 
 <section id="htb">
     <h1 class="ms-5 mt-5">Pembelian Menu Kantin</h1>
     <?php
-    $conn = new mysqli("localhost", "root", "", "kantin");
+    $conn = new mysqli("localhost", "root", "", "kantin1");
     if ($conn->connect_error) {
         die("Koneksi gagal: " . $conn->connect_error);
     }
@@ -151,7 +226,7 @@ Es Durian
     $sql = "SELECT * FROM menu";
     $result = $conn->query($sql);
 
-    $kategori = ["Kantin Bu Ida" => ["Katsu", "Es"], "Kantin Pak Narji" => ["Bubur", "Kelapa", "Durian"]];
+    $kategori = ["Kantin Pak Rusdi" => ["Katsu", "Es"], "Kantin Bu Dwi" => ["", "",]];
     $kategoriSekarang = "";
     $kategoriTampil = [];
 
@@ -201,30 +276,38 @@ while ($row = $result->fetch_assoc()) {
 <div id="sdh"class="mt-3 mb-5 ms-5" style="display: none;">
   <button class="btn btn-primary" onclick="sudahBayar()">Sudah Bayar</button>
 </div>
-
-<section id ="contact">
-<h1 class="ms-5 mt-5">Contact Us</h1>
-<form>
-  <div class="mb-1 ms-5 mt-5 w-25">
-    <label for="exampleInputEmail1" class="form-label">Nama</label>
-    <input type="" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+<br>
+<br>
+<br>
+<section class="ms-5 mx-5"id="contact">
+  <form>
+  <div class="form-group w-50">
+    <label for="exampleInputEmail1">Nama</label>
+    <input class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    
   </div>
-   <div class="mb-1 ms-5 w-25">
-    <label for="exampleInputEmail1" class="form-label">Email</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  <div class="form-group w-50">
+    <label for="exampleInputPassword1">Email</label>
+    <input type="Email" class="form-control" id="exampleInputPassword1">
   </div>
-  <div class="mb-1 ms-5 w-25">
-  <label for="exampleFormControlTextarea1" class="form-label">Pesan</label>
+  <div class="mb-3 w-50">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
   <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 </div>
-  <div class="mt-3 ms-5 mb-5">
-  <button type="submit" class="btn btn-primary" onclick="alertform()">Submit</button>
-</div>
+  <button class="mt-4"class="btn btn-primary" onclick="sigmaAlert()">Submit</button>
 </form>
+<script>
+  function sigmaAlert(){
+    alert("Terimakasih!");
+  }
+</script>
 </section>
-<footer class="bg-primary text-white py-1 fixed-bottom text-center">
-  <p class="mb-0">© copyright najwan</p>
+<br>
+<br>
+<br>
+<footer class="fixed-bottom bg-primary text-center ">
+  <p>aku sigma</p>
 </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
