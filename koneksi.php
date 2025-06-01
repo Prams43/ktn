f2a63485f46cdd098e73d716539a8b387fa2078a
Prams43 <najwanhp123@gmail.com>
2025-06-01 23:08:29 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,25 +1,6 @@
 <?php
-$koneksi = new mysqli("localhost", "root", "", "kantin");
-
-if ($koneksi->connect_error) {
-    die("Koneksi gagal: " . $koneksi->connect_error);
-}
-
-$nama = $_POST['nama'];
-$harga = $_POST['harga'];
-
-if (!empty($nama) && !empty($harga)) {
-    $stmt = $koneksi->prepare("INSERT INTO transaksi (nama, harga) VALUES (?, ?)");
-    $stmt->bind_param("si", $nama, $harga);
-    if ($stmt->execute()) {
-        echo "success";
-    } else {
-        echo "gagal";
-    }
-    $stmt->close();
-} else {
-    echo "data tidak lengkap";
-}
-
-$koneksi->close();
+$conn = new mysqli("localhost", "root", "", "kantin");
+$conn->query("INSERT INTO transaksi (nama, harga) VALUES ('{$_POST['nama']}', {$_POST['harga']})")
+    ? print("success") : print("gagal");
+$conn->close();
 ?>
